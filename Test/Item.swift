3da2afa912041d391646b90dc6e6537eec38d395
Simon <sv87@rice.edu>
2026-09-11 22:05:06 -0500

    Initial Commit

---
+++ Test/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Test
+//
+//  Created by Simon Velandia on 9/11/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
