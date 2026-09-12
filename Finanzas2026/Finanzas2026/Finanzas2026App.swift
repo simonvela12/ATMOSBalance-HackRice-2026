faa077a109f6241505e1f75a47714c5803edd451
lucaschinif <lc201@rice.edu>
2026-09-12 17:00:27 -0500

    Launch the unified Forecast and Context product shell

--- Finanzas2026/Finanzas2026/Finanzas2026App.swift
+++ Finanzas2026/Finanzas2026/Finanzas2026App.swift
@@ -6,10 +6,7 @@ struct Finanzas2026App: App {
 
     var body: some Scene {
         WindowGroup {
-            // Accounts and the bank connection live behind the profile button in
-            // ContentView's top bar. There used to be a second floating button for
-            // the same thing stacked on top of the screen, which made both hard to hit.
-            ContentView()
+            ProductRootView()
                 .environmentObject(bankStore)
         }
     }
